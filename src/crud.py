1b93dc7465efa3ca9e1e4c4e9b65ebc0886fbc4d
balbesina228 <dolphie.guy@gmail.com>
2024-06-03 15:07:44 +0700

    add get_author query

--- src/crud.py
+++ src/crud.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from sqlalchemy.orm import Session
 
 from . import models
@@ -6,6 +8,10 @@ from . import schemas
 
 def get_authors(db: Session, skip: int = 0, limit: int = 30):
     return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author(db: Session, author_id: UUID):
+    return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
 def create_author(db: Session, author: schemas.AuthorCreate):
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+from uuid import UUID
+
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from . import crud
@@ -18,12 +20,20 @@ def get_db():
         db.close()
 
 
-@app.get("/authors")
+@app.get("/authors", response_model=list[schemas.Author])
 def read_authors(db: Session = Depends(get_db)):
     authors = crud.get_authors(db)
     return authors
 
 
-@app.post("/authors")
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def read_author(author_id: UUID, db: Session = Depends(get_db)):
+    author = crud.get_author(db, author_id=author_id)
+    if author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return author
+
+
+@app.post("/authors", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db, author)
